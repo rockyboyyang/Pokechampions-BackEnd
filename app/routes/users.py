5a142c11db972f406ebfb9a000751d289e3864b6
Rocky <yangrocky2013@gmail.com>
2020-08-14 18:41:32 -0500

    add misty, fix routes

--- app/routes/users.py
+++ app/routes/users.py
@@ -83,6 +83,8 @@ def edit_pokemon_user_info(userId, pokemonSlot):
         finStr = finStr.replace('ser"s', "ser's")
     if '\n\n' in finStr:
         finStr = finStr.replace('\n\n', " ")
+    if 'e"s' in finStr:
+        finStr = finStr.replace('e"s', "e's")
 
 
     if pokemonSlot == 'slot_1':
